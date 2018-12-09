5b26d3ea36a4fa20ca35303bc98c97bc2b9bfc1e
wolferine <tabriz94@gmail.com>
2018-12-09 17:31:12 +0700

    Initial Commit

---
+++ Filterize/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Filterize
+//
+//  Created by Tabriz Dzhavadov on 09/12/2018.
+//  Copyright © 2018 Tabriz Dzhavadov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Filterize/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Filterize
+//
+//  Created by Tabriz Dzhavadov on 09/12/2018.
+//  Copyright © 2018 Tabriz Dzhavadov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
